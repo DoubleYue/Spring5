cc239b831c0d51026f5cde28b9831ad54300e835
XiaoYue <1399590729@qq.com>
2022-03-10 21:51:40 +0800

    First Commit

---
+++ Spring01/src/com/example/spring/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.example.spring.test;
+
+/**
+ * @Author Yue
+ * @Date 2022/03/10
+ * @Description <p> </p>
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+
+    }
+}

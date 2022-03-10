dd0f4b27991592644d5015df496ce32ed3577523
XiaoYue <1399590729@qq.com>
2022-03-10 21:52:54 +0800

    Second Commit

--- Spring01/src/com/example/spring/test/GitTest.java
+++ Spring01/src/com/example/spring/test/GitTest.java
@@ -8,7 +8,8 @@ package com.example.spring.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("Hello Git");
+        System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
 
     }
 }
